1bdc0fc41f5ec52484463e989ad26ccd8b6c4973
Arani Inothma <inothmaa@gmail.com>
2026-02-22 11:41:00 +0530

    Update testInstruction.service.js
    add the function and filers option to search instruction by test name, instructions and language.

--- apps/backend/src/modules/lab/testInstruction.service.js
+++ apps/backend/src/modules/lab/testInstruction.service.js
@@ -5,6 +5,59 @@ export const createTestInstruction = async (data) =>{
     return testInstruction.save();
 };
 export const findTestInstructionByTestTypeId = async (testTypeId) => {
-    return TestInstruction.findById(testTypeId);
+    return TestInstruction.findOne({ diagnosticTestId: testTypeId });
 };
 
+export const getAllTestInstructions = async (filters = {}) => {
+    const query = {};
+
+    // Filter by instruction content
+    if (filters.instructions) {
+        query.$or = [
+            { preTestInstructions: { $regex: filters.instructions, $options: 'i' } },
+            { postTestInstructions: { $regex: filters.instructions, $options: 'i' } }
+        ];
+    }
+
+    // Filter by language
+    if (filters.languageCode) {
+        query.languageCode = filters.languageCode;
+    }
+
+    // Filter by test name
+    let populateOptions = {};
+    if (filters.testName) {
+        populateOptions = {
+            path: 'diagnosticTestId',
+            match: { name: { $regex: filters.testName, $options: 'i' } }
+        };
+    } else {
+        populateOptions = { path: 'diagnosticTestId' };
+    }
+
+    return TestInstruction.find(query)
+        .populate(populateOptions)
+        .then(results => {
+            if (filters.testName) {
+                return results.filter(instr => instr.diagnosticTestId);
+            }
+            return results;
+        });
+};
+export const getTestInstructionById = async (id) => {
+    return TestInstruction.findById(id);
+};
+
+export const getTestInstructionsByLanguage = async (testTypeId, languageCode) => {
+    return TestInstruction.findOne({ diagnosticTestId: testTypeId, languageCode });
+};
+
+export const getTestInstructionsByDiagnosticTestId = async (diagnosticTestId) => {
+    return TestInstruction.find({ diagnosticTestId });
+};
+export const updateTestInstruction =async(id, updateData) =>{
+    return TestInstruction.findByIdAndUpdate(id, updateData, {new: true});
+};
+export const deleteTestInstruction = async (id) => {
+    return TestInstruction.findByIdAndDelete(id);
+}
